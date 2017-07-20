89d2c0f08287b3cce6d919f7cf649401d756aa19
stealkiller <frankrosario1998@gmail.com>
2017-07-20 15:14:08 -0400

    Add project files.

---
+++ MFSFinalProject/ViewModel/ProductViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MFSFinalProject.Infra;
+using System.Collections.ObjectModel;
+using MFSFinalProject.Model;
+
+
+namespace MFSFinalProject.ViewModel
+{
+    public class ProductViewModel : NotificationClass
+    {
+        private ObservableCollection<Product> products;
+        public ProductViewModel()
+        {
+            LoadProduct();
+        }
+        public void LoadProduct()
+        {
+            using (MFSContext context = new MFSContext())
+            {
+                Products = new ObservableCollection<Model.Product>(context.Products.ToList());
+            }
+        }
+
+        public ObservableCollection<Product> Products
+        {
+            get { return products; }
+            
+            set
+            {
+                products = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+    }
+}
