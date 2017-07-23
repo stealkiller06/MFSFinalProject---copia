a8e18d61ecebb6e7f8055ee73bec360ffab20ef1
stealkiller <frankrosario1998@gmail.com>
2017-07-23 19:48:14 -0400

    Mantenimiento Measurement Completado.

---
+++ MFSFinalProject/View/MeasurementView.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MFSFinalProject.ViewModel;
+
+namespace MFSFinalProject.View
+{
+    /// <summary>
+    /// Interaction logic for MeasurementView.xaml
+    /// </summary>
+    public partial class MeasurementView : Window
+    {
+        public MeasurementView()
+        {
+            InitializeComponent();
+            this.DataContext = new MeasurementViewModel();
+        }
+
+        private void TextBoxSearchBar_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void DataGridCategories_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void DataGridMeasurements_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ MFSFinalProject/ViewModel/MeasurementViewModel.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MFSFinalProject.Infra;
+using MFSFinalProject.Model;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace MFSFinalProject.ViewModel
+{
+    public class MeasurementViewModel : NotificationClass
+    {
+        private Measurement selectedMeasurement;
+        private ObservableCollection<Measurement> measurements;
+        public MeasurementViewModel()
+        {
+            LoadMeasurements();
+            selectedMeasurement = new Measurement();
+            DeleteMeasurementCommand = new MyICommand(OnDelete, CanDelete);
+            UpdateCatecoryCommand = new MyICommand(OnUpdateMeasurement, CanUpdateMeasurement);
+            AddMeasurementCommand = new MyICommand(OnAddMeasurement, CanAddMeasurement);
+        }
+
+        public void LoadMeasurements()
+        {
+            ObservableCollection<Measurement> measurements;
+            using (MFSContext context = new MFSContext())
+            {
+                measurements = new ObservableCollection<Measurement>(context.Measurements.ToList().Where(c => c.Remove != 1));
+            }
+            Measurements = measurements;
+        }
+
+        public ObservableCollection<Measurement> Measurements
+        {
+            get { return measurements; }
+            set
+            {
+                measurements = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Measurement SelectedMeasurement
+        {
+            get { return selectedMeasurement; }
+            set
+            {
+                selectedMeasurement = value;
+                OnPropertyChanged();
+                DeleteMeasurementCommand.RaiseCanExecuteChanged();
+                UpdateCatecoryCommand.RaiseCanExecuteChanged();
+            }
+
+        }
+
+        #region ICommand members
+        /// <summary>
+        /// Command to delete a Measurement
+        /// </summary>
+        public MyICommand DeleteMeasurementCommand { get; set; }
+
+        public void OnDelete()
+        {
+            MessageBoxResult result =
+           MessageBox.Show("¿Estás seguro de eliminar la categoría '" + SelectedMeasurement.Name + "'?", "Mensaje de confirmación",
+                            MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.No)
+                return;
+
+            SelectedMeasurement.Remove = 1;
+            OnUpdateMeasurement();
+
+            MessageBox.Show("La categoria fue eliminada satisfactoriamente.");
+
+        }
+        public bool CanDelete()
+        {
+            try
+            {
+                //if the category has a name that mean is a category already saved
+                if (SelectedMeasurement.Name != null)
+                    return true;
+            }
+            catch (System.Exception)
+            {
+                return false;
+
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Command to update a Measurement
+        /// </summary>
+        public MyICommand UpdateCatecoryCommand { get; set; }
+
+        public void OnUpdateMeasurement()
+        {
+            using (MFSContext context = new MFSContext())
+            {
+                context.Entry(selectedMeasurement).State = (selectedMeasurement.MeasurementId == 0) ?
+                                                        System.Data.Entity.EntityState.Added :
+                                                        System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+            LoadMeasurements();
+
+        }
+
+        public bool CanUpdateMeasurement()
+        {
+
+            return SelectedMeasurement != null;
+        }
+
+        /// <summary>
+        /// Command to add a category
+        /// </summary>
+        public MyICommand AddMeasurementCommand { get; set; }
+
+        public void OnAddMeasurement()
+        {
+            SelectedMeasurement = new Measurement();
+            OnPropertyChanged("Measurements");
+        }
+
+        public bool CanAddMeasurement()
+        {
+            return true;
+        }
+        #endregion
+    }
+}
