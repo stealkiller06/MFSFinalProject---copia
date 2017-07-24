3a3edd51d7ebe492b9ddfc9158f638d80db45ad1
stealkiller <frankrosario1998@gmail.com>
2017-07-24 19:42:45 -0400

    Editando OrderView.

---
+++ MFSFinalProject/Model/Help/OrderAux.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MFSFinalProject.Model.Help
+{
+    public class OrderAux
+    {
+        public int OrderID { get; set; }
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public int SuplierId { get; set; }
+        public string SuplierName { get; set; }
+        public DateTime Date { get; set; }
+        public string CodOrder { get; set; }
+    }
+}
---
+++ MFSFinalProject/Model/Order.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MFSFinalProject.Infra;
+
+namespace MFSFinalProject.Model
+{
+    public class Order 
+    {
+
+        #region Properties
+        public int OrderId { get; set; }
+        public DateTime Date { get; set; }
+        public string CodOrder { get; set; }
+        #endregion
+
+        #region Relationships
+        public virtual Suplier Suplier { get; set; }
+        public virtual User User { get; set; }
+        #endregion
+
+
+    }
+}
